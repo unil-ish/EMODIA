461df6e48a136435601a89bdbd721a7707de318f
Virgile Albasini <virgile.albasini@unil.ch>
2024-04-30 10:54:39 +0200

    création de la classe

--- conversation.py
+++ conversation.py
@@ -1,5 +1,6 @@
 #code de la classe conversation par virgile
 import pandas as pd
+
 class Conversation:
     def __init__(self,id,movie_id,character_id,lines):
         self.id = id
@@ -7,8 +8,15 @@ class Conversation:
         self.character_id = character_id
         self.lines = lines()
 
+    @classmethod
+    def create_dataframe(cls):
+        created_dataframe = pd.DataFrame()
+        return created_dataframe
 
-        @classmethod
-        def create_dataframe():
-            created_dataframe = pd.DataFrame()
-            return created_dataframe
+    @classmethod
+    def sentiment_dynamics(cls,sentiment_data):
+        raise NotImplementedError
+
+    @classmethod
+    def speech_analysis(cls):
+        raise NotImplementedError
---
+++ line.py
@@ -0,0 +1,7 @@
+#code de la classe line par virgile
+class Line:
+    def __init__(self,id,movie_id,character_id,content):
+        self.id = id
+        self.movie_id = movie_id
+        self.character_id = character_id
+        self.lines = content
