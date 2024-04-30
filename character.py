9b51b17baefcf76199e1da04131683f6501dc548
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-04-30 11:18:41 +0200

    created character class

--- character.py
+++ character.py
@@ -1,1 +1,9 @@
+##code de sophie pour la classe Characters
 
+class Character:
+    def __init__(self, character_id, name, movie_id, gender, credits_position):
+        self.character_id = character_id
+        self.name = name
+        self.movie_id = movie_id
+        self.gender = gender
+        self.credits_position = credits_position
