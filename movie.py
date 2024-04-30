c43ef8b6ff4b4800fea0cbfd9359c5a0d13c9f48
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-04-30 10:56:02 +0200

    created class movie

--- movie.py
+++ movie.py
@@ -1,1 +1,12 @@
+#code de sophie pour la classe Movie
+import pandas as pd
+class Movie:
+    def __init__(self, id, title, release_year, rating, votes):
+        #id function: id()
+        self.id = id
+        self.title = title
+        self.release_year = release_year
+        self.rating = rating
+        self.votes = votes
+        self.genres = []
 
