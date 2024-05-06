06b55ed131346b8a40556ab8ce02eff8a386fc96
Virgile Albasini <virgile.albasini@unil.ch>
2024-05-06 17:30:45 +0200

    avancement des classes

--- conversation.py
+++ conversation.py
@@ -1,12 +1,13 @@
-#code de la classe conversation par virgile
+# code de la classe conversation
 import pandas as pd
+
 
 class Conversation:
     def __init__(self,id,movie_id,character_id,lines):
         self.id = id
         self.movie_id = movie_id
         self.character_id = character_id
-        self.lines = lines()
+        self.lines = lines
 
     @classmethod
     def create_dataframe(cls):
@@ -20,3 +21,34 @@ class Conversation:
     @classmethod
     def speech_analysis(cls):
         raise NotImplementedError
+
+
+# le chemin vers movie_conversations.tsv
+tsv_file_path = 'movie_dialog/movie_conversations.tsv'
+# on crée un identifiant unique, car le tsv n'en contient pas
+conversation_id = 0
+
+# Ouvre le tsv ligne par ligne en encodage utf8 comme movie_conversations.tsv, un peu lent mais fonctionne
+with open(tsv_file_path, 'r', encoding="utf8") as tsvfile:
+    # on boucle sur chaque ligne du tsv
+    for line in tsvfile:
+
+        # pour tester le bon fonctionnement
+        # print(line)
+
+        # Sépare les entrées grâce à la tabulation "\t"
+        parts = line.strip().split('\t')
+
+        # Crée un objet Conversation et lui attribue les données splitées
+        # la première conversation serait : u0	u2	m0	['L194' 'L195' 'L196' 'L197']
+        # S'il y a un problème, toute la ligne de données devient "?",
+        try:
+            conversation_obj = Conversation(conversation_id, parts[2], {"character1":parts[0],"character2":parts[1]}, parts[3])
+        except:
+            conversation_obj = Conversation(conversation_id,"?","?","?")
+
+        # pour tester
+        # print(conversation_obj.id, conversation_obj.movie_id, conversation_obj.character_id, conversation_obj.lines)
+
+        # on incrémente conversation_id de 1
+        conversation_id = conversation_id+1
--- line.py
+++ line.py
@@ -1,7 +1,41 @@
-#code de la classe line par virgile
+# code de la classe line
 class Line:
-    def __init__(self,id,movie_id,character_id,content):
-        self.id = id
+    # initialisation dans le même ordre que les données dans movie_line.tsv
+    def __init__(self, line_id, movie_id, character_id, character_name, line_content):
+        self.line_id = line_id
         self.movie_id = movie_id
         self.character_id = character_id
-        self.lines = content
+        self.character_name = character_name
+        self.line_content = line_content
+
+
+# le chemin vers movie_lines.tsv
+# tous les guillemets("") de movie_lines.tsv ont été supprimés
+tsv_file_path = 'movie_dialog/movie_lines.tsv'
+
+# On crée une liste pour stocker les Line, utile seulement pour l'affichage actuellement
+line_objects = []
+
+# Ouvre le tsv ligne par ligne en encodage utf8 comme movie_lines.tsv, un peu lent mais fonctionne
+with open(tsv_file_path, 'r', encoding="utf8") as tsvfile:
+    # on boucle sur chaque ligne du tsv
+    for line in tsvfile:
+
+        # Sépare les entrées grâce à la tabulation "\t"
+        parts = line.strip().split('\t')
+
+        # Crée un objet Line et lui attribue les données splitées
+        # la première ligne serait : L1045	u0	m0	BIANCA	They do not!
+        # S'il y a un problème, toute la ligne de données devient "?",
+        # par exemple, il manque le line_content à certains endroits, mais c'est un montant négligeable
+        try:
+            line_obj = Line(parts[0], parts[2], parts[1], parts[3], parts[4])
+        except:
+            line_obj = Line("?", "?","?","?","?")
+
+        # on ajoute la ligne à la liste de lignes
+        line_objects.append(line_obj)
+
+# line_objects contient toutes les instances de Line, pour tester si ça fonctionne
+# for line_obj in line_objects:
+#     print(line_obj.line_id, line_obj.character_id, line_obj.movie_id, line_obj.character_name, line_obj.line_content)
