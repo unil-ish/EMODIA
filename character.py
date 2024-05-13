269b08c3e5270ad6610b635b1a6ee8831c78b3a3
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-05-13 22:55:25 +0200

    changes to movie and character class

--- character.py
+++ character.py
@@ -8,11 +8,40 @@ class Character:
         self.gender = gender
         self.credits_position = credits_position
 
-    def gender(self):
-        pass
+    def get_name(self):
+        name_list = []
+        merged_list = dict(zip([id], name_list))  # id = character id
+        return merged_list
 
-    def credits_position(self):
-        pass
+    def get_movie_id(self):
+        movie_id_list = []
+        merged_list = dict(zip([id], movie_id_list))
+        return merged_list
+    def get_gender(self):
+        gender_list = []
+        merged_list = dict(zip([id], gender_list))
+        return merged_list
+    def get_credits_position(self):
+        credits_list = []
+        merged_list = dict(zip([id], credits_list))
+        return merged_list
+        # [id] ?
+
+    @property
+    def get_all_names(self):
+        return self.get_all_names
+    @property
+    def get_all_movie_id(self):
+        return self.get_all_movie_id
+    @property
+    def get_all_gender(self):
+        return self.get_all_gender
+    @property
+    def get_all_credits_position(self):
+        return self.get_all_credits_position
+    @property
+    def get_all_characters(self):
+        return self.get_all_characters
 
 character_df = pd.read_csv('movie_dialog/movie_characters_metadata.tsv', sep=',') #why works with ',' and not '\t' ?
 print(character_df)
--- movie.py
+++ movie.py
@@ -2,17 +2,48 @@
 import pandas as pd
 class Movie:
     def __init__(self, id, title, release_year, rating, votes, genres):
-        #id function: id()
+        # id function: id()
         self.id = id
         self.title = title
         self.release_year = release_year
         self.rating = rating
         self.votes = votes
-        #self.genres = [] ?
+        # self.genres = [] ?
         self.genres = genres
 
-    def genres(self):
+        self.all_id = [] # get all movie id, inside class or outside?
+
+    def get_title(self):
+        title_list = []
+        merged_list = dict(zip([id], title_list)) #id = movie id
+        return merged_list
+    def get_release_year(self):
+        release_list = []
+        merged_list = dict(zip([id], release_list))
+        return merged_list
+    def get_rating(self):
+        rating_list = []
+        merged_list = dict(zip([id], rating_list))
+        return merged_list
+    def get_votes(self):
+        votes_list = []
+        with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                if len(parts) >= 6:
+                    #genres are in the 6th column
+                    votes_str = parts[4]
+                    #take away individual [], all votes go into one []
+                    votes_str = votes_str.strip('[]')
+                    for vote in votes_str.split(','):
+                        votes_list.append(vote)
+        return votes_list
+
+        merged_list = dict(zip([id], votes_list))
+        return merged_list
+    def get_genres(self):
         genres_list = []
+        #we don't use then?
         with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
             for line in file:
                 parts = line.strip().split('\t')
@@ -24,20 +55,32 @@ class Movie:
                     for genre in genres_str.split(','):
                         genres_list.append(genre)
         return genres_list
+        #or put movie_id in () method ?
+        #pour faire une liste de dictionnaires?
+        merged_list = dict(zip([id], genres_list))
+        return merged_list
 
-    def release_year(self):
-        pass
-
-    def rating(self):
-        pass
-
-    def votes(self):
-        pass
-
+    #all properties, methode doesnt do an action, can access attributes
+    @property
+    def get_all_title(self):
+        return self.get_all_title
+    @property
+    def get_all_release_year(self):
+        return self.get_all_release_year
+    @property
+    def get_all_rating(self):
+        return self.get_all_rating
+    @property
+    def get_all_votes(self):
+        return self.get_all_votes
+    @property
+    def get_all_genres(self):
+        return self.get_all_genres
 
 
 movie_df = pd.read_csv('movie_dialog/movie_titles_metadata.tsv', sep='\t')
 print(movie_df)
 
 movie = Movie('m0', '10 things i hate about you', 1999, 6.90, 62847, [])
-print(Movie.genres('movie_dialog/movie_titles_metadata.tsv'))
+print(Movie.get_genres('movie_dialog/movie_titles_metadata.tsv'))
+print(Movie.get_votes('movie_dialog/movie_titles_metadata.tsv'))
