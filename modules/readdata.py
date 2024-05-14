34ea8f951a2fcf4889fe9372ba31fc36a20c73ff
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-05-14 11:10:12 +0200

    added unzip to readdata

--- modules/readdata.py
+++ modules/readdata.py
@@ -1,4 +1,18 @@
+import zipfile
+
 def readdata(path):
+    if '.zip' in path.name:
+        print('that\'s a zip!')
+        try:
+            test = zipfile.ZipFile(path, 'r')
+            return_data = []
+            for filename in test.namelist():
+                with zipfile.ZipFile(path) as my_zip:
+                    with my_zip.open(filename) as data:
+                        return_data.append(data.read())
+            return return_data
+        except:
+            print('unzip error')
     try:
         with path.open('r') as file:
             data = file.read()
@@ -13,4 +27,4 @@ def readdata(path):
         elif not path.exists():
             print('Path does not exist.')
 
-
+#def parsed_data(path):
