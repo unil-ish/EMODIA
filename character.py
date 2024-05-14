a128c134a392837f08fe37be64b927517653a47b
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-05-14 13:22:42 +0200

    changes character class: put into comments code from before (just in case). updated code

--- character.py
+++ character.py
@@ -11,36 +11,31 @@ class Character:
         self.credits_position = credits_position
 
         # do we not have to put them in __init__() ?
-        # self.get_all_id = [] --> but can we do this if there is no method ?
+        self.get_all_id = [] # --> but can we do this if there is no method ?
         self.get_all_name = []
         self.get_all_movie_id = []
         self.get_all_gender = []
         self.get_all_credits_position = []
 
     def get_name(self):
-        name_list = []
-        merged_list = dict(zip([id], name_list))  # id = character id
-        return merged_list
+        merged_list = dict(zip(self.get_all_id, self.get_all_name))
+        return merged_list[id]
 
     def get_movie_id(self):
-        movie_id_list = []
-        merged_list = dict(zip([id], movie_id_list))
-        return merged_list
+        merged_list = dict(zip(self.get_all_id, self.get_all_movie_id))
+        return merged_list[id]
 
     def get_gender(self):
-        gender_list = []
-        merged_list = dict(zip([id], gender_list))
-        return merged_list
+        merged_list = dict(zip(self.get_all_id, self.get_all_gender))
+        return merged_list[id]
 
     def get_credits_position(self):
-        credits_list = []
-        merged_list = dict(zip([id], credits_list))
-        return merged_list
-        # [id] ?
+        merged_list = dict(zip(self.get_all_id, self.get_all_list))
+        return merged_list[id]
 
     @property
-    def get_all_names(self):
-        return self.get_all_names
+    def get_all_name(self):
+        return self.get_all_name
 
     @property
     def get_all_movie_id(self):
