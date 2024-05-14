4f1321bd5cd4d5d18f33c69263170e4b6c5696bd
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-05-14 10:36:50 +0200

    Update readdata with error messages.

--- modules/readdata.py
+++ modules/readdata.py
@@ -1,4 +1,16 @@
 def readdata(path):
-    with path.open('r') as file:
-        data = file.read()
-        return data
+    try:
+        with path.open('r') as file:
+            data = file.read()
+            return data
+    except:
+        if path.is_file():
+            print('File exists but read error.')
+        elif path.is_dir():
+            print('Directory exists. Please input file and not dir.')
+        elif path.exists():
+            print('Path exists, but unexpected error.')
+        elif not path.exists():
+            print('Path does not exist.')
+
+
