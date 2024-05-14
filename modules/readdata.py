98c3b859218286da179d8f97e8b913be942305e4
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-05-14 11:20:47 +0200

    Added zip_file kwarg to readdata

--- modules/readdata.py
+++ modules/readdata.py
@@ -1,15 +1,26 @@
 import zipfile
 
-def readdata(path):
+
+def readdata(path, **kwargs):
     if '.zip' in path.name:
+        if not kwargs["zip_name"]:
+            print('Please select a zip file.')
+            return
+        zip_name = kwargs['zip_name']
+        print(f'desired file in zip: {zip_name}')
         print('that\'s a zip!')
         try:
             test = zipfile.ZipFile(path, 'r')
             return_data = []
-            for filename in test.namelist():
-                with zipfile.ZipFile(path) as my_zip:
-                    with my_zip.open(filename) as data:
-                        return_data.append(data.read())
+            print(test.namelist())
+            #for filename in test.namelist():
+            #    with zipfile.ZipFile(path) as my_zip:
+            #        with my_zip.open(filename) as data:
+            #            return_data.append(data.read())
+            with zipfile.ZipFile(path, 'r') as my_zip:
+                with my_zip.open(zip_name) as data:
+                    return_data = data.read()
+
             return return_data
         except:
             print('unzip error')
@@ -27,4 +38,4 @@ def readdata(path):
         elif not path.exists():
             print('Path does not exist.')
 
-#def parsed_data(path):
+# def parsed_data(path):
