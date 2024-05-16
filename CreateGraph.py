c8a64639f5a195090229c1f625f4da90758f62b7
erica-brztg <145543547+erica-brztg@users.noreply.github.com>
2024-05-16 10:36:26 +0200

    Update and rename basegraph.py to CreateGraph.py

--- CreateGraph.py
+++ CreateGraph.py
@@ -1,32 +1,17 @@
-from abc import ABC, abstractmethod
-#importe la classe 'ABC' et le décorateur abstractmethod du module abc, qui sont utilisés pour définir une classe abstraite et des méthodes abstraites respectivement.
 import pandas as pd #ce sera importé avant mais il ne faut pas l'oublier
 import seaborn as sns
 #seaborn est plus adaptée aux dataframe et est construite au-dessus de matplot. De plus, elle offre moins de possibilités de custom, ce qui simplifie le code. Si on voulait des graphiques plus complexes, il faudrait peut-être travailler avec matplot.
 import matplotlib.pyplot as plt
 
-#Déclaration de la classe abstraite BaseGraph qui hérite de la classe ABC.
-class BaseGraph(ABC): 
+#Déclaration de la classe CreateGraph qui va créer les graphiques demandés
+class CreateGraph: 
     
     #constructeur de la classe BaseGraph, avec les paramètres qui seront utilisés pour initialiser les attributs de la classe
     def __init__(self, title, xlabel, ylabel):
         self.title = "Graph title goes here"
         self.xlabel = "x-axis label goes here"
         self.ylabel = "y-axis label goes here"
- 
-         
-    #def xy_values(self, data):
-        #pass
     
-    #@abstractmethod 
-    #def type(self, data):
-        #pass
-    
-    #@abstractmethod
-    #def show(self, data):
-        #pass
-    
-    @abstractmethod
     def create_graph(self, data, graph_type, **kwargs):
         if graph_type == 'scatter':
             self.create_scatterplot(data, **kwargs)
@@ -51,7 +36,7 @@ class BaseGraph(ABC):
         plt.ylabel(self.ylabel)
         plt.show()
         
-    def create_heatmap(self, data, x , y, value):
+    def create_heatmap(self):
         pivot_table = data.pivot_table(values = values, index = y, columns = x)
         sns.heatmap(data=pivot_table, cmap="coolwarm")
         plt.title(self.title)
@@ -62,16 +47,19 @@ class BaseGraph(ABC):
 # exemples d'utilisations à implémenter dans la fonction main: 
 
 # Création d'une instance de la classe abstraite avec un titre approprié
-#graph = BaseGraph(title='Title', xlabel='X', ylabel='Y')
+#graph = CreateGraph(title='Title', xlabel='x', ylabel='y')
 
 #charger les données des DataFrame créé dans les autres classes: 
-#data_exemple = exemple_df()
+#exemple_pd = pd.read_csv('exemple.pd')
 
 # Création d'un scatterplot en utilisant la méthode create_graph: il faut modifier le nom du df
-#graph.create_graph(data=data_exemple, graph_type='scatter', x='ID', y='Score')
+#graph.create_graph(data=exemple_pd, graph_type='scatter', x='ID', y='Score')
+
+#exemple pour le graphique release_year: 
+#graph.create_graph(data=df_release_year, graph_type='histogram', column='release_year')
 
 # Création d'un histogramme en utilisant la méthode create_graph: il faut modifier le nom du df
-#graph.create_graph(data=data_exemple, graph_type='histogram', column='Score')
+#graph.create_graph(data=exemple_pd, graph_type='histogram', column='Score')
 
 # Création d'une carte de chaleur en utilisant la méthode create_graph: il faut modifier le nom du df
-#graph.create_graph(data=data_exemple, graph_type='heatmap', x='ID', y='Titre', values='Score')
+#graph.create_graph(data=exemple_pd, graph_type='heatmap', x='ID', y='Titre', values='Score')
