1268d8a683adee83bf2be6a97f052f49ff1c490d
erica-brztg <145543547+erica-brztg@users.noreply.github.com>
2024-05-16 10:27:18 +0200

    Update createdataframe.py

--- modules/createdataframe.py
+++ modules/createdataframe.py
@@ -1,2 +1,8 @@
 def hello_world():
     print('hello world! (but from inside a function this time :)')
+
+#my_df = pd.DataFrame({'Data1': data1, 'Data2': data2, 'DataN': dataN})
+
+#exemple d'utilisation pour créer le dataframe pour les années de sortie:
+
+#df_release_year = pd.DataFrame({'Movie_title': title, 'Release_year': release_year})
