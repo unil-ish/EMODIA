943fc87e053d86582ee1d96e9008c693410395ec
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-05-17 15:22:03 +0200

    changes to movie.py to try another way

--- movie.py
+++ movie.py
@@ -1,156 +1,108 @@
-# code pour la classe Movie
 import pandas as pd
 
 
 class Movie:
-    def __init__(self, id, title, release_year, rating, votes, genres):
-        # id function: id()
-        self.id = id
+    def __init__(self, movie_id, title, release_year, rating, votes, genres):
+        self.movie_id = movie_id
         self.title = title
         self.release_year = release_year
         self.rating = rating
         self.votes = votes
-        # self.genres = [] ?
         self.genres = genres
 
-        self.all_id = [] # get all movie id, inside class or outside?
-        self.all_title = []
-        self.get_all_release_year = []
-        self.get_all_rating = []
-        self.get_all_votes = []
-        self.get_all_genres = []
+    def get_title(self):
+        return self.title
 
-    def get_title(self, id):
-        # title_list = []
-        # merged_list = dict(zip([id], title_list)) #id = movie id
-        # return merged_list
-
-        # create a dict by zipping two lists together
-        merged_list = dict(zip(self.get_all_id, self.get_all_title))
-        return merged_list[id]
-
-    def get_release_year(self, id):
-        merged_list = dict(zip(self.get_all_id, self.get_all_release_year))
-        return merged_list[id]
+    def get_release_year(self):
+        return self.release_year
 
     def get_rating(self):
-        # return self.rating --> *
-        merged_list = dict(zip(self.get_all_id, self.get_all_rating))
-        return merged_list[id]
+        return self.rating
 
     def get_votes(self):
-        # votes_list = []
-        # with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
-        #    for line in file:
-        #        parts = line.strip().split('\t')
-        #        if len(parts) >= 6:
-        #            #genres are in the 6th column
-        #            votes_str = parts[4]
-        #            #take away individual [], all votes go into one []
-        #            votes_str = votes_str.strip('[]')
-        #            for vote in votes_str.split(','):
-        #                votes_list.append(vote)
-        # return votes_list
-
-        merged_list = dict(zip(self.get_all_id, self.get_all_votes))
-        return merged_list[id]
+        return self.votes
 
     def get_genres(self):
-        # genres_list = []
-        # we don't use then?
-        # with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
-        #    for line in file:
-        #        parts = line.strip().split('\t')
-        #        if len(parts) >= 6:
-                    #genres are in the 6th column
-        #            genres_str = parts[5]
-        #            #take away individual [], all genres go into one []
-        #            genres_str = genres_str.strip('[]')
-        #            for genre in genres_str.split(','):
-        #                genres_list.append(genre)
-        # return genres_list
-        # or put movie_id in () method ?
-        # pour faire une liste de dictionnaires?
-        merged_list = dict(zip(self.get_all_id, self.get_all_genres))
-        return merged_list[id]
-
-    # all properties, method doesn't do an action, can access attributes
-    # gives a list of all titles, all release years, etc.
-    @property
-    def _all_id(self):
-        return self.all_id
-
-    @property
-    def _all_title(self):
-        return self.get_all_title
-
-    @property
-    def _all_release_year(self):
-        return self.get_all_release_year
-
-    @property
-    def _all_rating(self):
-        return self.get_all_rating
-
-    @property
-    def _all_votes(self):
-        return self.get_all_votes
-
-    @property
-    def _all_genres(self):
-        return self.get_all_genres
+        return self.genres
 
     @classmethod
-    def get_all_title(cls, id_list):
-        merged_list = dict(zip(Movie._all_id, Movie._all_title))
-        # pourquoi pas all_title = [] comme plus haut ?
+    def read_tsv(cls, file_path):
+        """Read the TSV file and return its contents as a list."""
+        data = []
+        with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                if len(parts) >= 6:  # the file has at least 6 fields
+                    data.append(parts)
+        return data
+
+    @classmethod
+    def get_all_title(cls):
+        data = cls.read_tsv('movie_dialog/movie_titles_metadata.tsv')
         list_all_title = []
-        for ids in id_list:
-            list_all_title.append(merged_list[ids])
+        for parts in data:
+            title_str = parts[1]
+            title_str = title_str.strip('[]')
+            for title in title_str.split(','):
+                list_all_title.append(title)
         return list_all_title
 
     @classmethod
-    def get_all_release_year(cls, id_list):
-        merged_list = dict(zip(Movie._all_id, Movie._all_release_year))
+    def get_all_release_year(cls):
+        data = cls.read_tsv('movie_dialog/movie_titles_metadata.tsv')
         list_all_release_year = []
-        for ids in id_list:
-            list_all_release_year.append(merged_list[ids])
+        for parts in data:
+            release_year_str = parts[2]
+            release_year_str = release_year_str.strip('[]')
+            for release_year in release_year_str.split(','):
+                list_all_release_year.append(release_year)
         return list_all_release_year
 
     @classmethod
-    def get_all_rating(cls, id_list):
-        merged_list = dict(zip(Movie._all_id, Movie._all_rating))
+    def get_all_rating(cls):
+        data = cls.read_tsv('movie_dialog/movie_titles_metadata.tsv')
         list_all_rating = []
-        for ids in id_list:
-            list_all_rating.append(merged_list[ids])
+        for parts in data:
+            rating_str = parts[3]
+            rating_str = rating_str.strip('[]')
+            for rating in rating_str.split(','):
+                list_all_rating.append(rating)
         return list_all_rating
 
     @classmethod
-    def get_all_votes(cls, id_list):
-        merged_list = dict(zip(Movie._all_id, Movie._all_votes))
+    def get_all_votes(cls):
+        data = cls.read_tsv('movie_dialog/movie_titles_metadata.tsv')
         list_all_votes = []
-        for ids in id_list:
-            list_all_votes.append(merged_list[ids])
+        for parts in data:
+            votes_str = parts[4]
+            votes_str = votes_str.strip('[]')
+            for votes in votes_str.split(','):
+                list_all_votes.append(votes)
         return list_all_votes
 
     @classmethod
-    def get_all_genres(cls, id_list):
-        merged_list = dict(zip(Movie._all_id, Movie._all_genres))
+    def get_all_genres(cls):
+        data = cls.read_tsv('movie_dialog/movie_titles_metadata.tsv')
         list_all_genres = []
-        for ids in id_list:
-            list_all_genres.append(merged_list[ids])
+        for parts in data:
+            genres_str = parts[5]
+            genres_str = genres_str.strip('[]')
+            for genres in genres_str.split(','):
+                list_all_genres.append(genres)
         return list_all_genres
 
 
-# movie_df = pd.read_csv('movie_dialog/movie_titles_metadata.tsv', sep='\t')
-# print(movie_df)
+# with open('movie_dialog/movie_titles_metadata.tsv', 'r', encoding='utf-8') as file:
+#    print(Movie.get_all_title())
 
-# movie = Movie('m0', '10 things i hate about you', 1999, 6.90, 62847, [])
-# print(Movie.get_genres('movie_dialog/movie_titles_metadata.tsv'))
-# print(Movie.get_votes('movie_dialog/movie_titles_metadata.tsv'))
+# check it works
+# print(Movie.get_all_title())
+# print(Movie.get_all_release_year())
+# print(Movie.get_all_rating())
+# print(Movie.get_all_votes())
+# print(Movie.get_all_genres())
 
-tsv_file_path = 'movie_dialog/movie_titles_metadata.tsv'
-# title, speaker, speech : process_file.py
-movie_objects = []
-list_id = []
-list_
+test_df = pd.DataFrame(list(zip(Movie.get_all_title(), Movie.get_all_release_year(), Movie.get_all_rating(),
+                                Movie.get_all_votes(), Movie.get_all_genres())),
+                       columns=['Title', 'Release year', 'Rating', 'Votes', 'Genres'])
+print(test_df)
