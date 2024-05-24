e1e75ebf2d9ed2bf1a5815445585a1eeebc6c5a1
erica-brztg <145543547+erica-brztg@users.noreply.github.com>
2024-05-24 17:25:40 +0200

    Update CreateGraph.py

--- CreateGraph.py
+++ CreateGraph.py
@@ -2,6 +2,7 @@ import pandas as pd #ce sera importé avant mais il ne faut pas l'oublier
 import seaborn as sns
 #seaborn est plus adaptée aux dataframe et est construite au-dessus de matplot. De plus, elle offre moins de possibilités de custom, ce qui simplifie le code. Si on voulait des graphiques plus complexes, il faudrait peut-être travailler avec matplot.
 import matplotlib.pyplot as plt
+import networkx as nx
 
 #Déclaration de la classe CreateGraph qui va créer les graphiques demandés
 class CreateGraph: 
@@ -19,6 +20,14 @@ class CreateGraph:
             self.create_histogram(data, **kwargs)
         elif graph_type == 'heatmap':
             self.create_heatmap(data, **kwargs)
+        elif graph_type == 'stacked_bar_chart':
+            self.create_stacked_bar_chart(data, **kwargs)
+        elif graph_type == ('network_graph')
+            edges = kwargs.get(edges, character_list)
+            G = self.create_network_graph(edges, character_list)
+            self.visualize_network_graph(G)
+        else: 
+            print("Ce type de graphe n'est pas pris en compte par le programme :(")
             
     #on peut ajouter d'autres types de graph si besoin: ici on choisirait d'implémenter de nouvelles méthodes mais si on voit qu'il y a 56 types de graphiques, on pourrait créer des sous-classes
             
@@ -30,7 +39,7 @@ class CreateGraph:
         plt.show()
     
     def create_histogram(self, data, column):
-        sns.histplot(data=data[column])
+        sns.histplot(data=data[column], palette='tab10')
         plt.title(self.title)
         plt.xlabel(self.xlabel)
         plt.ylabel(self.ylabel)
@@ -42,6 +51,30 @@ class CreateGraph:
         plt.title(self.title)
         plt.xlabel(self.xlabel)
         plt.ylabel(self.ylabel)
+        plt.show()
+        
+    def create_stacked_bar_chart(self):
+        sns.countplot(data=data, x=x, hue=hue, palette='tab10')
+        plt.title(self.title)
+        plt.xlabel(self.xlabel)
+        plt.ylabel(self.ylabel)
+        plt.show()
+        
+    def create_network_graph(self, dialogue_list):
+        # Initialiser un graphe vide
+        G = nx.Graph()
+
+        # Ajouter les arêtes au graphe
+        G.add_edges_from(edges)
+
+        return G
+    
+    def visualize_network_graph(self, G):
+        # Afficher le graphe de réseau
+        plt.figure(figsize=(10, 6))
+        pos = nx.spring_layout(G)  # Choisir une disposition pour les nœuds
+        nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=1500, font_size=10, font_weight='bold', edge_color='gray')
+        plt.title(self.title)
         plt.show()
         
 # tentatives de création de graphiques 
