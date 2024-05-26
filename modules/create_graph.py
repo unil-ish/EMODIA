d91dfb252211002e87acf4f48f46015701dca017
erica-brztg <145543547+erica-brztg@users.noreply.github.com>
2024-05-26 16:16:35 +0200

    Update create_graph.py

--- modules/create_graph.py
+++ modules/create_graph.py
@@ -1,26 +1,28 @@
-import pandas as pd  # ce sera importé avant mais il ne faut pas l'oublier
+import pandas as pd 
 import seaborn as sns
-# seaborn est plus adaptée aux dataframe et est construite au-dessus de matplot. De plus, elle offre moins de possibilités de custom, ce qui simplifie le code. Si on voulait des graphiques plus complexes, il faudrait peut-être travailler avec matplot.
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# La libririe seaborn est plus adaptée aux dataframe et est construite au-dessus de matplot.
 
-# Déclaration de la classe CreateGraph qui va créer les graphiques demandés
+
 class CreateGraph:
+    """Déclaration de la classe CreateGraph pour créer différents types de graphiques"""
 
-    # constructeur de la classe BaseGraph, avec les paramètres qui seront utilisés pour initialiser les attributs de la classe
     def __init__(
             self,
             title="Graph title goes here",
             xlabel="x-axis label goes here",
             ylabel="y-axis label goes here",
     ):
-        sns.set_theme()  # Use default Seaborn theme.
+    """Initialisation de la classe CreateGraph avec en paramètres le titre et le label des axe x et y"""
+        sns.set_theme()  # utilise le thème par défaut de Seaborn
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
 
     def create_graph(self, data, graph_type, **kwargs):
+    """Créer un graphique en fonction du type demandé"""
         # Todo : match: case
         # match graph_type:
         #   case: 'scatter'
@@ -45,9 +47,10 @@ class CreateGraph:
         else:
             print("Ce type de graphe n'est pas pris en compte par le programme :(")
 
-    # on peut ajouter d'autres types de graph si besoin: ici on choisirait d'implémenter de nouvelles méthodes mais si on voit qu'il y a 56 types de graphiques, on pourrait créer des sous-classes
+    #Il est possible d'ajouter d'autres types de graphiques: 1. Déclarer un nouveau type de graphique et définir ses paramètres 2. L'ajouter à la méthde de création de grahpiques
 
     def create_scatterplot(self, data, x, y, hue=None, palette='magma_r', color=None):
+    """Création d'un scatterplot."""
         sns.scatterplot(x=x, y=y, data=data, hue=hue, color=color)
         plt.title(self.title)
         plt.xlabel(self.xlabel)
@@ -56,6 +59,7 @@ class CreateGraph:
         plt.show()
 
     def create_histogram(self, data, column, palette=None, bins='auto', discrete=True, color='coral'):
+    """Création d'un histogramme."""
         with sns.color_palette(palette):
             sns.histplot(data=data[column], discrete=discrete, bins=bins, color=color)
         plt.title(self.title)
@@ -65,6 +69,7 @@ class CreateGraph:
         plt.show()
 
     def create_heatmap(self):
+    """Création d'une carte de chaleur."""
         pivot_table = data.pivot_table(values=values, index=y, columns=x)
         sns.heatmap(data=pivot_table, cmap="coolwarm")
         plt.title(self.title)
@@ -73,6 +78,7 @@ class CreateGraph:
         plt.show()
 
     def create_bar_chart(self, data, x,  hue=None, y=None, orient='v', order=None, rotation=0, palette=None, color=None):
+    """Création d'un graphique en barres."""
         plot = sns.catplot(kind='bar', data=data, x=x, y=y, hue=hue, palette=palette, orient=orient, order=order, color=color)
         plot.set_xticklabels(rotation=rotation)
         plt.tight_layout()
@@ -82,6 +88,7 @@ class CreateGraph:
         plt.show()
 
     def create_dist_chart(self, data, x, hue=None, y=None, palette=None, color=None):
+    """Création d'un graphique de distribution."""
         sns.histplot(data=data, x=x, y=y, hue=hue, palette=palette, log_scale=False,
                            color=color, stat='proportion', cumulative=True, common_norm=False, fill=False, element='step')
         plt.tight_layout()
@@ -91,6 +98,7 @@ class CreateGraph:
         plt.show()
 
     def create_box_chart(self, data, x, hue=None, y=None, palette=None, color=None):
+    """Création d'un boxplot."""
         #sns.boxplot(data=data, x=x, y=y, hue=hue, palette=palette, color=color, showfliers=False, width=.5, gap=.2)
         sns.catplot(kind='violin', data=data, x=x, y=y, hue=hue, split=True, log_scale=True, gap=0.1, inner=None, palette='Pastel2')
         plt.tight_layout()
@@ -100,6 +108,7 @@ class CreateGraph:
         plt.show()
 
     def create_network_graph(self, dialogue_list):
+    """Création d'un grah de réseau."""
         # Initialiser un graphe vide
         G = nx.Graph()
 
@@ -109,6 +118,7 @@ class CreateGraph:
         return G
 
     def visualize_network_graph(self, G):
+    """Affichage du graph de réseau"""
         # Afficher le graphe de réseau
         plt.figure(figsize=(10, 6))
         pos = nx.spring_layout(G)  # Choisir une disposition pour les nœuds
