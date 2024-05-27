790327a1fec8568fa7a82d35b7f72185a448eb6b
Virgile Albasini <virgile.albasini@unil.ch>
2024-05-27 19:14:59 +0200

    création des commentaires avec """ google doc

--- modules/conversation.py
+++ modules/conversation.py
@@ -12,46 +12,103 @@ class Conversation:
     all_lines_id = []  # une liste de liste de tous les id de line
 
     def __init__(self, conversation_id, characters_id, movie_id, lines):
+        """ initialise la conversation
+        Args:
+            conversation_id (int) : id de la conversation, int car on doit le créer nous-même
+            characters_id ({str}) : dictionnaire d'id de personnages
+            movie_id (str) : id de film
+            lines ([str]) : liste d'id de lignes
+        """
         self.id = conversation_id
         self.characters_id = characters_id  #un dictionnaire qui se présente ainsi : {"character1":u1,"character2":u2}
         self.movie_id = movie_id
         self.lines = lines
 
-    # les getter qui prennent un conversation_id en input, pour retourner la valeur cherchée correspondante
     def get_characters_id(self, conversation_id):
-        # on crée un dictionnaire à partir de deux listes : la liste de toutes les id de conversation et la liste
-        # des dictionnaires des id de personnages
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        on crée un dictionnaire à partir de deux listes : la liste de toutes les id de conversation et la liste
+        des dictionnaires des id de personnages, on cherche la valeur associée à la clé fournie
+        Args:
+            line_id (str) : id de la conversation
+        Returns:
+            character_id (str) : id de personnage
+        Examples:
+            >> conversation_id = 3
+            >> get_characters_id(conversation_id)
+            "u23"
+        """
         merged_list = dict(zip(self.all_conversations_id, self.all_characters_id))
         return merged_list[conversation_id]
 
     def get_movie_id(self, conversation_id):
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        Args:
+            line_id (str) : id de la conversation
+        Returns:
+            movie_id (str) : id de film
+        Examples:
+            >> conversation_id = 3
+            >> get_movie_id(conversation_id)
+            "m1"
+        """
         merged_list = dict(zip(self.all_conversations_id, self.all_movies_id))
         return merged_list[conversation_id]
 
     def get_lines(self, conversation_id):
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        Args:
+            line_id (str) : id de la conversation
+        Returns:
+            line_id ([str]) : liste d'id de lignes
+        Examples:
+            >> conversation_id = 3
+            >> get_lines(conversation_id)
+            "['L194' 'L195' 'L196' 'L197']"
+        """
         merged_list = dict(zip(self.all_conversations_id, self.all_lines_id))
         return merged_list[conversation_id]
 
     # on crée des @property pour pouvoir accéder aux attributs
     @property
     def _all_characters_id(self):
+        """
+        permet de récupérer la liste de dictionnaires des id de personnages
+        """
         return self.all_characters_id
 
     @property
     def _all_movies_id(self):
+        """
+        permet de récupérer la liste des id de film
+        """
         return self.all_movies_id
 
     @property
     def _all_lines_id(self):
+        """
+        permet de récupérer la liste des id de lignes
+        """
         return self.all_lines_id
 
     @property
     def _all_conversations_id(self):
+        """
+        permet de récupérer la liste des id de conversations
+        """
         return self.all_conversations_id
 
-    # les classmethod pour accéder à des listes d'éléments voulus, demande une liste de id conversation
     @classmethod
     def get_list_characters(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de conversations
+        Returns:
+            list_characters ([str]) : liste d'ids de personnages
+        Examples:
+            >> id_list = [1,2,3]
+            >> get_list_characters(id_list)
+            [{1:"u1",2:"u2"},{1:"u3",2:"u4"},{1:"u5",2:"u6"}]
+        """
         merged_list = dict(zip(Conversation.all_conversations_id, Conversation.all_characters_id))
         list_characters = []
         for ids in id_list:
@@ -60,22 +117,51 @@ class Conversation:
 
     @classmethod
     def get_list_movie_ids(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de conversations
+        Returns:
+            list_movies ([str]) : liste d'ids de films
+        Examples:
+            >> id_list = [1,2,3]
+            >> get_list_movie_ids(id_list)
+            ["m1,m2,m3"]
+        """
         merged_list = dict(zip(Conversation.all_conversations_id, Conversation.all_movies_id))
-        list_all_movies = []
+        list_movies = []
         for ids in id_list:
-            list_all_movies.append(merged_list[ids])
-        return list_all_movies
+            list_movies.append(merged_list[ids])
+        return list_movies
 
     @classmethod
     def get_list_lines(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de conversations
+        Returns:
+            list_lines ([str]) : liste d'ids de lignes
+        Examples:
+            >> id_list = [1,2,3]
+            >> get_list_lines(id_list)
+            ["L1,L2,L3"]
+        """
         merged_list = dict(zip(Conversation.all_conversations_id, Conversation.all_lines_id))
-        list_all_lines = []
+        list_lines = []
         for ids in id_list:
-            list_all_lines.append(merged_list[ids])
-        return list_all_lines
+            list_lines.append(merged_list[ids])
+        return list_lines
 
     @staticmethod
     def create_conversation_dataset(provided_data):
+        """lit les données du dataset et créé des objets Conversation
+        Args:
+            provided_data (str) : les données lues
+        Returns:
+
+        Examples:
+            >> provided_data = "u0	u2	m0	['L194' 'L195' 'L196' 'L197']"
+            >> Line.create_conversation_dataset(provided_data)
+        """
         # Using enumerate so we don't have to increment a variable.
         for index, line in enumerate(provided_data.splitlines()):
             parts = line.split('\t')
--- modules/line.py
+++ modules/line.py
@@ -11,45 +11,104 @@ class Line:
     all_lines_content = []
 
     def __init__(self, line_id, movie_id, character_id, line_content):
+        """ initialise la ligne
+        Args:
+            line_id (str) : id de la ligne
+            movie_id (str) : id de film
+            character_id (str) : id de personnage
+            line_content (str) : contenu de la ligne
+        """
         self.id = line_id
         self.character_id = character_id
         self.movie_id = movie_id
         self.line_content = line_content
 
-    # les getter qui prennent un line_id en input, pour retourner la valeur cherchée correspondante
     @classmethod
     def get_character_id(cls, line_id):
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        on crée un dictionnaire à partir de deux listes : la liste de toutes les id de conversation et la liste
+        des id de personnages, on cherche la valeur associée à la clé fournie
+        Args:
+            line_id (str) : id de la ligne
+        Returns:
+            character_id (str) : id du personnage
+        Examples:
+            >> line_id = L3
+            >> get_character_id(line_id)
+            u23
+        """
         merged_list = dict(zip(cls.all_lines_id, cls.all_characters_id))
         return merged_list[line_id]
 
     def get_movie_id(self, line_id):
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        Args:
+            line_id (str) : id de la ligne
+        Returns:
+            character_id (str) : id du film
+        Examples:
+            >> line_id = L3
+            >> get_movie_id(line_id)
+            m12
+        """
         merged_list = dict(zip(self.all_lines_id, self.all_movies_id))
         return merged_list[line_id]
 
     def get_line_content(self, line_id):
+        """ un getter qui prend un id en input, pour retourner la valeur cherchée correspondante
+        Args:
+            line_id (str) : id de la ligne
+        Returns:
+            character_id (str) : id du personnage
+        Examples:
+            >> line_id = L3
+            >> get_line_content(line_id)
+            "bla bla"
+        """
         merged_list = dict(zip(self.all_lines_id, self.all_lines_content))
         return merged_list[line_id]
 
-    # on crée des @property pour pouvoir accéder aux attributs
     @property
     def _all_lines_id(self):
+        """
+        permet de récupérer la liste des id de lignes
+        """
         return self.all_lines_id
 
     @property
     def _all_characters_id(self):
+        """
+        permet de récupérer la liste des id de personnages
+        """
         return self.all_characters_id
 
     @property
     def _all_movies_id(self):
+        """
+        permet de récupérer la liste des id de film
+        """
         return self.all_movies_id
 
     @property
     def _all_lines_content(self):
+        """
+        permet de récupérer la liste des contenus de lignes
+        """
         return self.all_lines_content
 
     # les classmethod pour accéder à des listes d'éléments voulus, demande une liste de id line
     @classmethod
     def get_list_characters_id(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de ligne
+        Returns:
+            list_characters_id ([str]) : liste d'ids de personnages
+        Examples:
+            >> id_list = [L1,L2,L3]
+            >> get_list_characters_id(id_list)
+            [u1,u2,u3]
+        """
         merged_list = dict(zip(Line.all_lines_id, Line.all_characters_id))
         list_characters_id = []
         for ids in id_list:
@@ -58,6 +117,16 @@ class Line:
 
     @classmethod
     def get_list_movies_id(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de lignes
+        Returns:
+            list_movie_id ([str]) : liste d'ids de films
+        Examples:
+            >> id_list = [L1,L2,L3]
+            >> get_list_movies_id(id_list)
+            [m1,m2,m3]
+        """
         merged_list = dict(zip(Line.all_lines_id, Line.all_movies_id))
         list_movie_id = []
         for ids in id_list:
@@ -66,17 +135,33 @@ class Line:
 
     @classmethod
     def get_list_contents_id(cls, id_list):
+        """ un getter qui prend une liste d'id en input, pour retourner une liste de valeurs correspondantes
+        Args:
+            id_list ([str]) : une liste d'ids de lignes
+        Returns:
+            list_content ([str]) : liste des contenus de lignes
+        Examples:
+            >> id_list = [L1,L2,L3]
+            >> get_list_contents_id(id_list)
+            ["bla1","bla2","bla3"]
+        """
         merged_list = dict(zip(Line.all_lines_id, Line.all_lines_content))
         list_content = []
         for ids in id_list:
             list_content.append(merged_list[ids])
         return list_content
 
-# utilisé pour la lecture des données
     @staticmethod
     def create_line_dataset(provided_data):
-        # fills Line class variables from read_data
+        """lit les données du dataset et créé des objets Line
+        Args:
+            provided_data (str) : les données lues
+        Returns:
 
+        Examples:
+            >> provided_data = "L1045	u0	m0	BIANCA	They do not!"
+            >> Line.create_line_dataset(provided_data)
+        """
         for line in provided_data.splitlines():
             parts = line.split('\t')
 
