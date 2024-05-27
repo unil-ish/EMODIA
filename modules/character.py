7ff4830a82e47347d58488373c0559e29786a1a2
Sophwrd <114563221+Sophwrd@users.noreply.github.com>
2024-05-27 20:34:03 +0200

    update des commentaires format google docstring dans movie.py et character.py

--- modules/character.py
+++ modules/character.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class Character:
     # attributs de classe
-    all_character_objects = []
+    all_character_objects = []  # une liste qui contient tous les objets de character
     all_characters_id = []  # une liste de tous les id de character
     all_names_id = []
     all_movies_id = []
@@ -18,49 +18,82 @@ class Character:
         self.gender_id = gender_id
         self.credits_position_id = credits_position_id
 
-    # donner un name_id associé à un character_id
     def get_name_id(self, character_id):
-        # mettre deux listes ensemble et mettre cette liste dans un dictionnaire
+        """Retourne un ID de nom associé à un ID de personnage
+
+            Args :
+                character_id : un string qui contient l'ID de character.
+
+            Returns :
+                Mettre ensemble all_characters_id et all_names_id avec un zip. Ensuite,
+                mettre ceci dans un dictionnaire nommé merged_list. Avec cela, on peut
+                utiliser l'ID d'un personnage pour chercher son nom correspondant.
+        """
         merged_list = dict(zip(self.all_characters_id, self.all_names_id))
         # cherche l'id character donné et retourne l'id name associé
         return merged_list[character_id]
 
     def get_movie_id(self, character_id):
+        """Retourne un ID de movie associée à un ID de character"""
         merged_list = dict(zip(self.all_characters_id, self.all_movies_id))
         return merged_list[character_id]
 
     def get_gender_id(self, character_id):
+        """Retourne un ID de genre associé à un ID de character"""
         merged_list = dict(zip(self.all_characters_id, self.all_genders_id))
         return merged_list[character_id]
 
     def get_credits_position_id(self, character_id):
+        """Retourne un ID de position dans les crédits associée à un ID de character"""
         merged_list = dict(zip(self.all_characters_id, self.all_credits_positions_id))
         return merged_list[character_id]
 
     # création de @property pour pouvoir accéder aux attributs (avant __init__)
     @property
     def _all_names_id(self):
+        """Accéder à l'attribut de classe all_names_id
+
+            Returns :
+            La création d'une méthode avec un @property permet d'accéder
+            aux attributs de classe
+        """
         return self.all_names_id
 
     @property
     def _all_movies_id(self):
+        """Accéder à l'attribut de classe all_movies_id"""
         return self.all_movies_id
 
     @property
     def _all_genders_id(self):
+        """Accéder à l'attribut de classe all_genders_id"""
         return self.all_genders_id
 
     @property
     def _all_credits_positions_id(self):
+        """Accéder à l'attribut de classe all_credits_positions_id"""
         return self.all_credits_positions_id
 
     @property
     def _all_characters_id(self):
+        """Accéder à l'attribut de classe all_characters_id"""
         return self.all_characters_id
 
     # création de @classmethod
     @classmethod
     def get_all_names(cls, id_list):
+        """Une liste d'ID de personnages pour une liste de noms
+
+            Args :
+            cls : @classmethod qui accède aux attributs de classe.
+            id_list : une liste d'ID de personnages.
+
+            Returns :
+            merged_list contient un dictionnaire avec un zip contenant
+            tous les ID de personnages et tous les ID de noms. list_all_names est
+            une liste vide qui contiendra les noms qui correspondent aux IDs
+            dans id_list (les ID de character).
+        """
         merged_list = dict(zip(Character._all_characters_id, Character._all_names_id))  # dictionnaire qui met
         # ensemble les listes all_characters_id and all_names_id
         list_all_names = []
@@ -70,8 +103,8 @@ class Character:
 
     @classmethod
     def get_all_movies_id(cls, id_list):
-        merged_list = dict(zip(Character._all_characters_id, Character._all_movies_id))  # dictionnaire qui met
-        # ensemble les listes all_characters_id and all_names_id
+        """Une liste d'ID de personnages pour une liste de films"""
+        merged_list = dict(zip(Character._all_characters_id, Character._all_movies_id))
         list_all_movies = []
         for ids in id_list:
             list_all_movies.append(merged_list[ids])
@@ -79,8 +112,8 @@ class Character:
 
     @classmethod
     def get_all_genders_id(cls, id_list):
-        merged_list = dict(zip(Character._all_characters_id, Character._all_genders_id))  # dictionnaire qui met
-        # ensemble les listes all_characters_id and all_names_id
+        """Une liste d'ID de personnages pour une liste de genres"""
+        merged_list = dict(zip(Character._all_characters_id, Character._all_genders_id))
         list_all_genders = []
         for ids in id_list:
             list_all_genders.append(merged_list[ids])
@@ -88,6 +121,7 @@ class Character:
 
     @classmethod
     def get_all_credits_positions_id(cls, id_list):
+        """Une liste d'ID de personnages pour une liste de positions dans les crédits"""
         merged_list = dict(
             zip(Character._all_characters_id, Character._all_credits_positions_id))  # dictionnaire qui met
         # ensemble les listes all_characters_id and all_names_id
@@ -98,16 +132,36 @@ class Character:
 
     @staticmethod
     def create_character_dataset(provided_data):
-        for line in provided_data.splitlines():
+        """Remplir les variables de la classe Character depuis read_data
+
+            Args :
+                provided_data : châine de caractères contenant des données
+                tabulées où chaque ligne représente un personnage.
+
+            Returns :
+                La méthode sépare le string de provided_data en parties et extrait les
+                attributs individuels (character_id, name_id, movie_id, gender_id,
+                credits_position_id). Ensuite, la méthode essaie de convertir la partie
+                credits_position en integer. Si cela est réussie, la méthode vérifie si
+                credits_pos = 1000 (une valeur qui a peu de chances de se produire) alors
+                credits_pos sera remplacé par ?. 'Except' garantie que même si la ligne
+                ne peut pas être convertie ou s'il y a une erreur, 'credits_pos' aura
+                tout de même une valeur.
+                La méthode instancie de nouveaux objets de Character les ajoutent dans
+                une liste nommée 'all_character_objects'. Par cela, la méthode met à jour
+                les attributs en gardant des listes séparées pour chaque attribut.
+        """
+        for line in provided_data.splitlines():  # lignes sont divisées en parties
             line = str(line)
             parts = line.split('\t')
 
+            # essaie de convertir en integer
             try:
-                credits_pos = int(parts[5])
-                if credits_pos == 1000:
+                credits_pos = int(parts[5])  # 6ᵉ partie
+                if credits_pos == 1000:  # valeur default (qui a peut de chances de se produire)
                     credits_pos = '?'
             except:
-                credits_pos = '?'
+                credits_pos = '?'  # si exception (ex : ligne ne peut pas être convertie en int)
 
             entries = {
                 'character_id': parts[0],
--- modules/movie.py
+++ modules/movie.py
@@ -1,17 +1,25 @@
 # code pour la classe Movie
 import pandas as pd
+"""From string import digits, ascii_letters
+
+    On importe des constants spécifiques du module string.
+    digits est un string contenant des chiffres (0 à 9).
+    ascii_letters est un string contenant des lettres en
+    miniscule et en majuscule (abc...ABC...). 
+
+"""
 from string import digits, ascii_letters
 
 
 class Movie:
     # attributs de classe
-    all_movies_objects = []
+    all_movies_objects = []  # une liste qui contient tous les objets de movie
     all_movies_id = []  # une liste de tous les id de movie
     all_titles_id = []
     all_release_years_id = []
     all_ratings_id = []
     all_votes_id = []
-    all_genres_id = []
+    all_genres_id = []  # liste de listes de genres de chaque film
 
     def __init__(self, movie_id, title_id, release_year_id, ratings_id, votes_id, genres_id):
         self.movie_id = movie_id  # id d'un movie en particulier
@@ -21,61 +29,92 @@ class Movie:
         self.votes_id = votes_id
         self.genres_id = genres_id
 
-    # méthodes getter, donner un title_id associé à un movie_id
     def get_title_id(self, movie_id):
-        # mettre deux listes ensemble et mettre cette liste dans un dictionnaire
+        """Retourne un ID de titre associé à un ID de movie
+
+        Args :
+            movie_id : un string qui contient l'ID de movie.
+
+        Returns :
+            Mettre ensemble all_movies_id et all_titles_id avec un zip. Ensuite,
+            mettre ceci dans un dictionnaire nommé merged_list. Avec cela, on peut
+            utiliser l'ID d'un movie pour chercher son titre correspondant.
+        """
         merged_list = dict(zip(self.all_movies_id, self.all_titles_id))
         # cherche l'id movie donné et retourne l'id title associé
         return merged_list[movie_id]
 
     def get_release_year_id(self, movie_id):
+        """Retourne un ID de l'année de sortie associée à un ID de movie"""
         merged_list = dict(zip(self.all_movies_id, self.all_release_years_id))
         return merged_list[movie_id]
 
     def get_ratings_id(self, movie_id):
+        """Retourne un ID des notes associées à un ID de movie"""
         merged_list = dict(zip(self.all_movies_id, self.all_ratings_id))
         return merged_list[movie_id]
 
     def get_votes_id(self, movie_id):
+        """Retourne un ID des votes associés à un ID de movie"""
         merged_list = dict(zip(self.all_movies_id, self.all_votes_id))
         return merged_list[movie_id]
 
     def get_genres_id(self, movie_id):
+        """Retourne un ID des genres associés à un ID de movie"""
         merged_list = dict(zip(self.all_movies_id, self.all_genres_id))
         return merged_list[movie_id]
 
-    # création de @property pour pouvoir accéder
-    # aux attributs de classe (avant __init__)
     @property
     def _all_titles_id(self):
+        """Accéder à l'attribut de classe all_titles_id
+
+            Returns :
+            La création d'une méthode avec un @property permet d'accéder
+            aux attributs de classe
+        """
         return self.all_titles_id
 
     @property
     def _all_release_years_id(self):
+        """Accéder à l'attribut de classe all_release_years_id"""
         return self.all_release_years_id
 
     @property
     def _all_ratings_id(self):
+        """Accéder à l'attribut de classe all_ratings_id"""
         return self.all_ratings_id
 
     @property
     def _all_votes_id(self):
+        """Accéder à l'attribut de classe all_votes_id"""
         return self.all_votes_id
 
     @property
     def _all_genres_id(self):
+        """Accéder à l'attribut de classe all_genres_id"""
         return self.all_genres_id
 
     @property
-    def _all_movies_id(self):  # liste all_movies_id
+    def _all_movies_id(self):
+        """Accéder à l'attribut de classe all_movies_id"""
         return self.all_movies_id
 
     # création de @classmethod
     @classmethod
     def get_all_titles(cls, id_list):
-        # listes all_movies_id et all_titles_id ensemble dans un dictionnaire
+        """Une liste d'ID de films pour une liste de titres
+
+            Args :
+            cls : @classmethod qui accède aux attributs de classe.
+            id_list : une liste d'ID de films.
+
+            Returns :
+            merged_list contient un dictionnaire avec un zip contenant
+            tous les ID de movie et tous les ID de titres. list_all_titles est
+            une liste vide qui contiendra les titres qui correspondent aux IDs
+            dans id_list (les ID de movie).
+        """
         merged_list = dict(zip(Movie._all_movies_id, Movie._all_titles_id))
-        # liste vide pour mettre les titres qui correspondent aux ids dans id_list
         list_all_titles = []
         for ids in id_list:
             # prendre titre associé à l'id et le met dans list_all_titles
@@ -84,6 +123,7 @@ class Movie:
 
     @classmethod
     def get_all_release_years(cls, id_list):
+        """Une liste d'ID de films pour une liste d'années de sortie"""
         merged_list = dict(zip(Movie._all_movies_id, Movie._all_release_years_id))
         list_all_release_years_id = []
         for ids in id_list:
@@ -92,6 +132,7 @@ class Movie:
 
     @classmethod
     def get_all_ratings_id(cls, id_list):
+        """Une liste d'ID de films pour une liste de notes"""
         merged_list = dict(zip(Movie._all_movies_id, Movie._all_ratings_id))
         list_all_ratings_id = []
         for ids in id_list:
@@ -100,6 +141,7 @@ class Movie:
 
     @classmethod
     def get_all_votes_id(cls, id_list):
+        """Une liste d'ID de films pour une liste de votes"""
         merged_list = dict(zip(Movie._all_movies_id, Movie._all_votes_id))
         list_all_votes_id = []
         for ids in id_list:
@@ -108,25 +150,42 @@ class Movie:
 
     @classmethod
     def get_all_genres_id(cls, id_list):
+        """Une liste d'ID de films pour une liste de genres"""
         merged_list = dict(zip(Movie._all_movies_id, Movie._all_genres_id))
         list_all_genres_id = []
         for ids in id_list:
             list_all_genres_id.append(merged_list[ids])
         return list_all_genres_id
 
-    @staticmethod
+    @staticmethod  #
     def create_movie_dataset(provided_data):
-        # fills Movie class variables from read_data
+        """ Remplir les variables de la classe Movie depuis read_data
 
-        for line in provided_data.splitlines():
-            parts = line.split('\t')
-            genres = parts[5].split(' ')
-            clean_genres = []
+                Args :
+                    provided_data : châine de caractères contenant des données
+                    tabulées où chaque ligne représente un film.
+
+                Returns :
+                    La méthode sépare le string de provided_data en parties et
+                    extrait les attributs individuels (movie_id, title_id,
+                    release_year_id, ratings_id, votes_id et genres_id). Ensuite,
+                    elle nettoie les genres afin qu'il n'y ait que des lettres.
+                    La méthode instancie de nouveaux objets de Movie et les ajoutent
+                    dans une liste nommée 'all_movie_objects'. Par cela, la méthode
+                    met à jour les attributs en gardant des listes séparées pour
+                    chaque attribut.
+        """
+
+        for line in provided_data.splitlines():  # split provided_data en lignes
+            parts = line.split('\t')  # split chaque ligne en parties, chaque partie représente un attribut de movie
+            genres = parts[5].split(' ')  # genres (partie 5) divisés par des espaces
+            clean_genres = []  # instancie une liste vide
             for entry in genres:
-                clean_genre = ''.join(l for l in entry if l in ascii_letters)
+                clean_genre = ''.join(l for l in entry if l in ascii_letters)  # garde seulement lettres ASCII
                 clean_genres.append(clean_genre)
             # TODO: add proper type handling with try or smth like that
             entries = {
+                # clé (attributs) : valeur
                 'movie_id': parts[0],
                 'title_id': parts[1],
                 'release_year_id': ''.join(d for d in parts[2] if d in digits),
@@ -136,9 +195,11 @@ class Movie:
             }
 
             Movie.all_movies_objects.append(
-                Movie(**entries)
+                # nouvelle instance de Movie avec valeurs du dictionnaire 'entries'
+                Movie(**entries)  # ** décompacter le dictionnaire
             )
 
+            # stock les listes respectives
             Movie.all_movies_id.append(entries['movie_id'])
             Movie.all_titles_id.append(entries['title_id'])
             Movie.all_release_years_id.append(entries['release_year_id'])
